500066bbf925f635d513b0dd43a609c3c0479e9b
Sarkash Talib <Sarkash.talib@gmail.com>
2021-09-26 00:38:34 +0100

    created appbar(navbar)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter, useHistory } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   let history = useHistory();
   return (
     <BrowserRouter history={history}>
-      <p>This is from app</p>
+      <Navbar />
     </BrowserRouter>
   );
 }
