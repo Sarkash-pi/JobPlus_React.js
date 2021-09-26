ba66de76305b226ccc152314af010b1a7ac25cf3
Sarkash Talib <Sarkash.talib@gmail.com>
2021-09-26 16:44:47 +0100

    Styling Appbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { AppBar, Badge, IconButton, Tab, Tabs, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Badge, Hidden, IconButton, Tab, Tabs, Toolbar, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 
 import SearchIcon from '@material-ui/icons/Search';
@@ -6,46 +6,89 @@ import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 import React from 'react'
 
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: '900px',
+    margin: '0 auto',
+    border: '1px yellow dotted'
+  }, 
+  iconsWrap: {
+    marginLeft: 'auto',
+    border: '1px red dotted',
+    '& .MuiButtonBase-root': {
+      marginLeft: '13px'
+    }, 
+    '& .MuiSvgIcon-root': {
+      fontSize: '25px'
+    }
+  },
+  tabs: {
+    '& .MuiTab-root': {
+      minWidth: 10, 
+      marginLeft: '10px'
+    }
+  },
+  hamburger: {
+    fontSize: '35px',
+    marginRight: '10px'
+  }
+}));
  
  export default function Navbar() {
+   const classes = useStyles();
    return (
      <Box>
        <AppBar position="static">
-          <Toolbar>
+          <Toolbar className={classes.container}>
+            <Hidden mdUp>  
+              <IconButton
+                edge='start'
+                color='inherit'
+                aria-label='menu'
+                >
+                <MenuIcon className={classes.hamburger} />
+              </IconButton>
+            </Hidden>
             <Typography component="h6">JOBPLUS</Typography>
-            <Tabs>
-              <Tab 
-                key={0}
-                label={'Home'}
-                component={Link}
-                to={'/'}
-              />
+            <Hidden smDown>
+              <Tabs value={0} className={classes.tabs}>
+                <Tab 
+                  key={0}
+                  label={'Home'}
+                  component={Link}
+                  to={'/'}
+                />
 
-              <Tab 
-                key={1}
-                label={'Job Listings'}
-                component={Link}
-                to={'/job-listings'}
-              />
+                <Tab 
+                  key={1}
+                  label={'Job Listings'}
+                  component={Link}
+                  to={'/job-listings'}
+                />
 
-              <Tab 
-                key={2}
-                label={'Job Applications'}
-                component={Link}
-                to={'/job-applications'}
-              />
-            </Tabs>
-            <Box>
+                <Tab 
+                  key={2}
+                  label={'Job Applications'}
+                  component={Link}
+                  to={'/job-applications'}
+                />
+              </Tabs>
+            </Hidden>
+            <Box className={classes.iconsWrap}>
               <IconButton size="small" component={Link} to={'/search'} color='inherit' edge='start'>
                 <SearchIcon />
               </IconButton>
 
               <IconButton size="small" component={Link} to={'/notfications'} color='inherit' edge='start'>
-                <Badge color="error" overlap="circle" variant="dot">
+                <Badge color="error" overlap="circular" variant="dot">
                   <NotificationsNoneIcon />
                 </Badge>
               </IconButton>
