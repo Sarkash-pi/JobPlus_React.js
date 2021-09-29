e0f36e3315ca2c6e9cc3afcf10142ab4b4c1c3a2
Sarkash Talib <Sarkash.talib@gmail.com>
2021-09-29 23:15:00 +0100

    created Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, useHistory } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter history={history}>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   );
 }
