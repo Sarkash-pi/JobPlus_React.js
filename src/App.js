307dc57a0fda994137f4f6d71a0bff1aa705c264
Sarkash Talib <Sarkash.talib@gmail.com>
2021-10-03 21:46:39 +0100

    page setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { BrowserRouter, useHistory } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Cookie from './components/Cookie';
+import Home from './pages/Home';
 
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
   return (
     <BrowserRouter history={history}>
       <Navbar />
+      <Switch>
+        <Route exact path='/' render={props => <Home {...props} />} />
+      </Switch>
       <Cookie />
       <Footer />
     </BrowserRouter>
